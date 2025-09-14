7ee947813318fce46325edb4557258be718a085f
AdrielCreativeMindLabs <aguijarro@creativemindlabs.com>
2025-09-14 20:04:45 +0100

    ya o ke?

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
